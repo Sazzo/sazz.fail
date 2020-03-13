92d281f5e90c3ce1508b19f945941babcfe68ba9
Sazz <39680458+Sazzo@users.noreply.github.com>
2020-03-13 14:51:55 -0300

    Renatinho Friday 1.0.0

---
+++ friday/main.js
@@ -0,0 +1,12 @@
+const date = new Date()
+const friday = date.getDay()
+
+if(!friday === 5) {
+  const renatinho = document.getElementById("renatinho").style.display = "none"
+  }
+if(friday === 5) {
+   const musica = document.getElementById("animesong")
+     musica.play();
+  const warning = document.getElementById("notToday").style.display = "none"
+}
+console.log(`Dia Atual: ${friday}`)
