e6ed9195b38d1c04bd8e2be7a86cf0a2361a2114
Sazz <39680458+Sazzo@users.noreply.github.com>
2020-03-13 16:46:15 -0300

    Fix.

--- friday/main.js
+++ friday/main.js
@@ -4,6 +4,7 @@ const friday = date.getDay()
 if(!friday === 5) {
   const renatinho = document.getElementById("renatinho").style.display = "none"
   const song = document.getElementById("notPlay").style.display = "none"
+  const musica = document.getElementById("animesong").style.display = "none"
   }
 if(friday === 5) {
    const musica = document.getElementById("animesong")
