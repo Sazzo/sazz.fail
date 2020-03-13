f3798fd629642e14c5f48df1910135d6d0ab6fb6
Sazz <39680458+Sazzo@users.noreply.github.com>
2020-03-13 16:45:20 -0300

    Fix.

--- friday/main.js
+++ friday/main.js
@@ -4,10 +4,10 @@ const friday = date.getDay()
 if(!friday === 5) {
   const renatinho = document.getElementById("renatinho").style.display = "none"
   const song = document.getElementById("notPlay").style.display = "none"
-  const warning = document.getElementById("notToday").style.display = "none"
   }
 if(friday === 5) {
    const musica = document.getElementById("animesong")
      musica.play();
+   const warning = document.getElementById("notToday").style.display = "none"
 }
 console.log(`Dia Atual: ${friday}`)
