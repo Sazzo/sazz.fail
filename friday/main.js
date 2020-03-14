c82e1be95e0f909f55625b613da3492154fa6996
Sazz <39680458+Sazzo@users.noreply.github.com>
2020-03-14 09:14:38 -0300

    Tentando arrumar lol

--- friday/main.js
+++ friday/main.js
@@ -1,7 +1,7 @@
 const date = new Date()
 const friday = date.getDay()
-const noFriday = [0,1,2,3,4,6]
-if(friday === noFriday) {
+
+if(!friday === 5) {
   const renatinho = document.getElementById("renatinho").style.display = "none"
   const song = document.getElementById("notPlay").style.display = "none"
   const musica = document.getElementById("animesong").style.display = "none"
